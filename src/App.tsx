607289596c8ac88a0713a117004580aeae992a8d
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-06-21 13:08:50 -0300

    Learning Componentes, Propriedades e Estado in React

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,36 @@
+import { Button } from './Components/Button';
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Button text='Conteudo do botão 1' />
+      <Button text='Conteudo do botão 2' />
+      <Button text='Conteudo do botão 3' />
+      <Button />
+      <Button> Texto children </Button>
+    </>
+  );
 }
+/*
+<>
+  <a href='#exemplo' target='_blank'>
+todo  <Button /> é um componente
+  </a>
+</> 
+*/
+
+/* 
+todo para atribuir valores as props 
+todo faz-se: myProp='valueToThisProp'
+todo é bem semelhante aos atributos HTML
+function App() {
+  return (
+    <>
+      <Button text='Conteudo do botão 1' />
+      <Button text='Conteudo do botão 2' />
+      <Button text='Conteudo do botão 3' />
+    </>
+  );
+} */
 
 export default App;
