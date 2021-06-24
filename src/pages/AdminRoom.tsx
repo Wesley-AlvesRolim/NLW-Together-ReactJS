c1fba0ce39382fb475051c17b3ce9b32b045c951
Wesley-AlvesRolim <walvesrolim@gmail.com>
2021-06-24 16:11:55 -0300

    Feature to remove a question by admin

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,12 +1,14 @@
 import { useParams } from 'react-router-dom';
 
 import LogoImg from '../assets/images/logo.svg';
+import DeleteImg from '../assets/images/delete.svg';
 import '../styles/room.scss';
 
 import { Button } from '../Components/button';
 import { RoomCode } from '../Components/room-code';
 import { Question } from '../Components/Question';
 import { useRoom } from '../hooks/useRoom';
+import { DbFirebase } from '../services/firebase';
 
 type paramsType = {
   id: string;
@@ -16,6 +18,17 @@ export const AdminRoom = () => {
   const params = useParams<paramsType>();
   const roomId = params.id;
   const { Title, questions } = useRoom(roomId);
+
+  async function handleDeleteQuestion(questionId: string) {
+    const isToRemove: boolean = window.confirm(
+      'Tem certeza que você quer apagar essa pergunta?',
+    );
+    if (isToRemove) {
+      await DbFirebase.ref(
+        'rooms/' + roomId + '/questions/' + questionId,
+      ).remove();
+    }
+  }
 
   return (
     <div id='page-room'>
@@ -46,7 +59,15 @@ export const AdminRoom = () => {
                 key={question.id}
                 content={question.content}
                 author={question.author}
-              />
+              >
+                <button
+                  onClick={() => {
+                    handleDeleteQuestion(question.id);
+                  }}
+                >
+                  <img src={DeleteImg} alt='Apague essa pergunta.' />
+                </button>
+              </Question>
             );
           })}
         </div>
